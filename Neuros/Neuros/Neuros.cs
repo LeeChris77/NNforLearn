a54340551de490e95ce0b115cd368aaef3782cf8
CHIEN-TSAN LEE <s16113219@outlook.com>
2019-08-03 20:31:25 +0800

    add

---
+++ Neuros/Neuros/Neuros.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Neuros
+{
+    public class CreateNeuros
+    {
+        public double NeurosCal(double[]x , double[]w)
+        {
+            double cal=0;
+            if(x.Length == w.Length)
+            {
+                for (int i = 0; i < x.Length; i++)
+                {
+                    cal += x[i] * w[i];
+                }
+            }
+            return cal;
+        }
+    }
+}
---
+++ Neuros/Neuros/ZControlBlock.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.IO;
+using Neuros;
+namespace NeurosMain
+{
+    class ControlBlock
+    {
+        static void Main(string[] args)
+        {
+            //string[] ChosseFunctionName = { "Identity", "BinaryStep", "Sigmoid" };
+            //StreamWriter sw = new StreamWriter(@"D:\Desktop\Activation_Data\" + ChosseFunctionName[0] + ".txt");
+            //Console.WriteLine(@"D:\Desktop\Activation_Data\" + ChosseFunctionName[0]);
+            //Random rnd = new Random();
+            double bias = 1;
+            double[] x = {bias, 0, 0 };
+            double[] w = {bias, 0, 1 };
+            //CreateNeuros nur = new CreateNeuros(x, w, 0.8, "sigmoid");
+        }
+    }
+}
