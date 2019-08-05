a7715c4cad4a8e64825bf368628b2f9c3b6e478e
CHIEN-TSAN LEE <s16113219@outlook.com>
2019-08-05 20:56:50 +0800

    add

---
+++ Neuros/Neuros/Perceptron.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Activation;
+namespace Neuros
+{/// <summary>
+ ///{0.0 ,0.5,-1.0 } => 4
+    ///{ 1, 2, 3 } => 0;
+    ///Relu
+    /// </summary>
+    class Perceptron
+    {
+        private double [] x;
+        private double [] w;
+        /// <summary>
+        /// ///////////////////
+        /// </summary>
+        private double s;
+        public double y;
+
+        public Perceptron(int num)
+        {
+            x = new double[num];
+            w = new double[num];
+        }
+
+
+
+        private void Summation()
+        {
+            s = 0.0;
+            for (int i = 0; i < x.Length; i++)
+            {
+                s += x[i] * w[i];
+            }
+        }
+
+        public void Activation()
+        {
+            ActivationLib tmp = new ActivationLib();
+            Summation();
+            y = tmp.ReLUFunction(s);
+        }
+
+        public void ReadInput(double [] arr)
+        {
+            Array.Copy(arr, x, x.Length);
+        }
+        public void ReadWeights(double[] arr)
+        {
+            Array.Copy(arr, w, w.Length);
+        }
+    }
+}
--- Neuros/Neuros/ZControlBlock.cs
+++ Neuros/Neuros/ZControlBlock.cs
@@ -7,14 +7,28 @@ namespace NeurosMain
     {
         static void Main(string[] args)
         {
-            //string[] ChosseFunctionName = { "Identity", "BinaryStep", "Sigmoid" };
+            double[] inp =  {0.0 ,0.5,-1.0 };
+            double[] inw = { 1, 2, 3 };
+            Perceptron p = new Perceptron(3);
+            p.ReadInput(inp);
+            p.ReadWeights(inw);
+            p.Activation();
+            Console.WriteLine(p.y);
+            Console.ReadKey();
+        }
+    }
+}
+
+/*
+  //string[] ChosseFunctionName = { "Identity", "BinaryStep", "Sigmoid" };
             //StreamWriter sw = new StreamWriter(@"D:\Desktop\Activation_Data\" + ChosseFunctionName[0] + ".txt");
             //Console.WriteLine(@"D:\Desktop\Activation_Data\" + ChosseFunctionName[0]);
             //Random rnd = new Random();
             double bias = 1;
-            double[] x = {bias, 0, 0 };
-            double[] w = {bias, 0, 1 };
+            double[] x = {1, 2, 3 };
+            double[] w = {0.1, 0.2, 0.3 };
+            NewNeuros neuro = new NewNeuros();
+            Console.WriteLine(neuro.OneNeuro(x, w, "Sigmoid"));
             //CreateNeuros nur = new CreateNeuros(x, w, 0.8, "sigmoid");
-        }
-    }
-}
+ 
+     */
